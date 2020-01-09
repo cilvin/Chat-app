2cd45e43631239e24b8908e795482599beac31a5
cilvin <cilvin33@gmail.com>
2020-01-08 17:51:14 -0800

    5.3 changes

---
+++ components/Chat.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+//import relevant components from react native
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import KeyboardSpacer from 'react-native-keyboard-spacer';
+// create Screen2 (Chat) class
+export default class Chat extends Component {
+  state = {
+    messages: []
+  };
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          text: 'Hi ' + this.props.navigation.state.params.userName + '. Have a nice chat!',
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    })
+  }
+  //define title in navigation bar
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.state.params.userName,
+    };
+  };
+  //appending new message to messages object
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+  //render components
+  render() {
+    return (
+      //fullscreen component
+      <View style={{ flex:1, backgroundColor: this.props.navigation.state.params.backgroundColor }}>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {Platform.OS === 'android' ? <KeyboardSpacer /> : null }
+      </View>
+    );
+  }
+};
+const styles = StyleSheet.create({
+});
